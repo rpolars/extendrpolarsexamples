3913a9fbbc7d9e2fea10ad69e6187d25bd309291
sorhawell <sorhawell@gmail.com>
2023-07-14 16:11:16 +0200

    half done

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,24 @@
 use extendr_api::prelude::*;
+use extendr_polars as ep;
+use polars::prelude as pl;
 
 /// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
-fn hello_world() -> &'static str {
-    "Hello world!"
+fn hello_world() -> Robj {
+    use pl::*;
+    let df = df! [
+        "names" => ["a", "b", "c"],
+        "values" => [1, 2, 3],
+        "values_nulls" => [Some(1), None, Some(3)]
+    ]
+    .unwrap();
+    let wdf = ep::WrapDataFrame(df);
+
+    let robj = wdf.export_stream().unwrap();
+
+    dbg!(wdf);
+    robj
 }
 
 // Macro to generate exports.
